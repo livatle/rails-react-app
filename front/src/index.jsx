15f44a0e48b9c0ca558ae32804eef065d22c4b8a
livatle <livliv09atle@gmail.com>
2022-02-01 23:51:53 +0900

    Add: device-token-authのコントローラーファイル等を作成

--- front/src/index.jsx
+++ front/src/index.jsx
@@ -1,21 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Auth0Provider } from "@auth0/auth0-react";
 import './assets/styles/index.css';
 import App from './App';
 
-const domain = process.env.REACT_APP_AUTH0_DOMAIN || ''
-const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID || ''
-
 
 ReactDOM.render(
-  <Auth0Provider
-    domain={domain}
-    clientId={clientId}
-    redirectUri={window.location.origin}
-  >
-    <App />
-  </Auth0Provider>,
+    <App />,
   document.getElementById('root')
 );
 
