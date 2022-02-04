5d05a15f892b2c6ed2f74b92878e977a3f28a401
livatle <livliv09atle@gmail.com>
2022-02-05 03:46:22 +0900

    Add: ユーザーと投稿のアソシエーション、ルーティングを設定

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_01_075014) do
+ActiveRecord::Schema.define(version: 2022_02_04_175127) do
 
   create_table "posts", force: :cascade do |t|
     t.string "name"
