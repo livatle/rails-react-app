b2f98732a34a03118584a5c4f8b31155ff2f3919
livatle <livliv09atle@gmail.com>
2022-02-09 01:13:17 +0900

    Add: Relationshipコントローラーとそのルートを定義

---
+++ api/app/controllers/api/v1/relationship_controller.rb
@@ -0,0 +1,11 @@
+class RelationshipController < ApplicationController
+    before_action :authenticate_user!
+
+    def create
+        current_api_v1_user.follow(params[:user_id])
+    end
+
+    def destroy
+        current_api_v1_user.unfollow(params[:user_id])
+    end
+end
