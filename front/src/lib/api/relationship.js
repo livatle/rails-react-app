2856e328e05ba8a353b0c2a416e4f79f66c0ab4d
livatle <livliv09atle@gmail.com>
2022-02-09 18:41:46 +0900

    Add: フォロー解除のapiリクエストを作成

--- front/src/lib/api/relationship.js
+++ front/src/lib/api/relationship.js
@@ -10,3 +10,13 @@ export const follow = () => {
       'uid': Cookies.get('_uid')
     }})
 };
+
+//削除
+export const unfollow = () => {
+    return client.delete('/relationship',
+    {headers: {
+        'access-token': Cookies.get('_access_token'),
+        'client': Cookies.get('_client'),
+        'uid': Cookies.get('_uid')
+    }})
+};
