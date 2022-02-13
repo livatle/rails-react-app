a64437ff808f78e82f2c2672881448d344280b3f
livatle <livliv09atle@gmail.com>
2022-02-13 22:33:36 +0900

    Add: お気に入り機能のクライアント処理を作成

--- front/src/components/FavoriteButton.jsx
+++ front/src/components/FavoriteButton.jsx
@@ -1,16 +1,36 @@
-import React from "react";
-import Button from '@mui/material/Button';
+import React, { useState, useEffect } from "react";
 
+import { favorite, unfavorite } from '../lib/api/post'
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 
 const FavoriteButton = () => {
+    const [isFavorite, setIsFavorite] = useState(false)
+    const handleClickFavoriteButton = async () => {
+        if (isFavorite === false) {
+            try {
+                const res = await favorite();
+                console.log(res.data)
+                setIsFavorite(true)
+            } catch (e) {
+            console.log(e);
+            }
+        } else {
+            try {
+                const res = await unfavorite();
+                console.log(res.data)
+                setIsFavorite()
+            } catch (e) {
+            console.log(e);
+            }
+        }
+    }
     return (
         <FavoriteBorderIcon 
+            onClick={()=> handleClickFavoriteButton()}
             sx={{color: "primary", display: "inline-block", ml: "2em"}}
             variant='contained'
             color='primary'
         />
     )
 }
-
 export default FavoriteButton
