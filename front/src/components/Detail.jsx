03ea6cafb5d6073bd4f2aeed5435bb183db5500c
livatle <livliv09atle@gmail.com>
2022-02-16 10:36:09 +0900

    Style: 投稿詳細画面のスタイルを修正

--- front/src/components/Detail.jsx
+++ front/src/components/Detail.jsx
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from "react";
 import {useNavigate, useParams} from 'react-router-dom'
 import { getDetail } from "../lib/api/post"; 
 
+
 import Table from "@mui/material/Table";
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
@@ -14,16 +15,10 @@ import { TableBody } from "@mui/material";
 import { FavoriteButton } from './index'
 
 const Detail = () => {
-
     const [data, setData] = useState({});
-    // { id: "1" }を取得する
     const query = useParams();
-    // 戻るボタン用
     const navigate = useNavigate();
     
-    // 画面描画時にidがundefinedだとデータ取得できないので
-    // 依存配列にidを入れて、idがundifined => 1と更新された時に
-    // useEffectの副作用を使い、処理をもう一度実行させる
     useEffect(() => {
         handleGetDetail(query);
     }, [query]);
@@ -40,13 +35,12 @@ const Detail = () => {
 
     return (
         <div className="c-grid">
-            <button onClick={() => navigate('/')}>戻る</button>
-            <TableContainer component={Paper} sx={{bgcolor: "#f5f5f5", mt: "2em"}}>
+            <TableContainer component={Paper} sx={{bgcolor: "#222A4F", mt: "2em"}}>
                 <Table>
                     <TableBody>
                             <TableRow>
-                                <TableCell align="center">
-                                    {data.content}
+                                <TableCell align="center"sx={{width: "80%"}}>
+                                    <p className="item">{data.content}</p>
                                 </TableCell>
                                 <TableCell align="center">
                                     <FavoriteButton />
