dfac5465fce96cbe93c5b79a06c6d4d5c41a5dfe
livatle <livliv09atle@gmail.com>
2022-03-04 20:24:54 +0900

    Ref: apiクライアントの処理をリファクタリング

--- front/src/lib/api/post.js
+++ front/src/lib/api/post.js
@@ -1,6 +1,5 @@
-// post.js
 import client from "./client"
-import Cookies from "js-cookie"
+import { headers } from "./client";
 
 // 一覧
 export const getList = () => {
@@ -9,69 +8,34 @@ export const getList = () => {
 
 // 詳細
 export const getDetail = (id) => {
-  return client.get(`/posts/${id}`,
-  {headers: {
-    'access-token': Cookies.get('_access_token'),
-    'client': Cookies.get('_client'),
-    'uid': Cookies.get('_uid')
-  }}
-)}
+  return client.get(`/posts/${id}`, headers)}
 
 // 新規作成
 export const createPost = (params) => {
-  return client.post('/posts', params,
-  {headers: {
-    'access-token': Cookies.get('_access_token'),
-    'client': Cookies.get('_client'),
-    'uid': Cookies.get('_uid')
-  }})
+  return client.post('/posts', params, headers)
 };
 
 // 更新
 export const updatePost = (id, params) => {
-  return client.patch(`/posts/${id}`, params, 
-  {headers: {
-    'access-token': Cookies.get('_access_token'),
-    'client': Cookies.get('_client'),
-    'uid': Cookies.get('_uid')
-  }});
+  return client.patch(`/posts/${id}`, params, headers);
 };
 
 // 削除
 export const deletePost = (id) => {
-  return client.delete(`/posts/${id}`,
-  {headers: {
-    'access-token': Cookies.get('_access_token'),
-    'client': Cookies.get('_client'),
-    'uid': Cookies.get('_uid')
-  }});
+  return client.delete(`/posts/${id}`, headers);
 };
 
 // 新規お気に入り作成
 export const favorite = (id, params) => {
-  return client.post(`/posts/${id}/favorites`, params,
-  {headers: {
-      'access-token': Cookies.get('_access_token'),
-      'client': Cookies.get('_client'),
-      'uid': Cookies.get('_uid')
-  }})
+  return client.post(`/posts/${id}/favorites`, params, headers)
 };
+
 // お気に入り解除
 export const unfavorite = (id) => {
-  return client.delete(`/posts/${id}/favorites`,
-  {headers: {
-      'access-token': Cookies.get('_access_token'),
-      'client': Cookies.get('_client'),
-      'uid': Cookies.get('_uid')
-  }})
+  return client.delete(`/posts/${id}/favorites`, headers)
 };
 
 //お気に入りしているかどうか
 export const checkFavorite = (id) => {
-  return client.get(`/posts/${id}`,
-  {headers: {
-      'access-token': Cookies.get('_access_token'),
-      'client': Cookies.get('_client'),
-      'uid': Cookies.get('_uid')
-  }})
+  return client.get(`/posts/${id}`, headers)
 };
