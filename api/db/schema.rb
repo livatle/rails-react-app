2e1176996928c26ed3a251cefdebac8794b89d4f
livatle <livliv09atle@gmail.com>
2022-03-07 05:52:21 +0900

    Add: postsモデルのnameカラムを削除

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_13_121643) do
+ActiveRecord::Schema.define(version: 2022_03_06_204519) do
 
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
@@ -20,7 +20,6 @@ ActiveRecord::Schema.define(version: 2022_02_13_121643) do
   end
 
   create_table "posts", force: :cascade do |t|
-    t.string "name"
     t.string "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
